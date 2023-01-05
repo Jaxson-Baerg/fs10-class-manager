375fcce0c7ad55554020eb93c264c4abcf025d56
Jaxson Baerg <jaxson.baerg@gmail.com>
2023-01-04 17:15:15 -0700

    Fixed register class button still appearing when a class is full and prevented server from processing register if the class is full.

--- server/routes/schedule.js
+++ server/routes/schedule.js
@@ -32,15 +32,19 @@ router.get('/:class_type_id', async (req, res) => {
 router.post('/register/confirm', async (req, res) => {
   try {
     if (req.session.user) {
-      if (req.session.user.credits >= req.body.credits) {
-        res.render('../../client/views/pages/class_register', { user: req.session.user, class_type_id: req.body.class_type_id, class_id: req.body.class_id, credits: req.body.credits });
-      } else {
-        const subscriptions = req.session.user.customer_id ? await stripe.subscriptions.list({
-          customer: req.session.user.customer_id
-        }) : { data: null };
+      if (req.body.spots_remaining > 0) {
+        if (req.session.user.credits >= req.body.credits) {
+          res.render('../../client/views/pages/class_register', { user: req.session.user, class_type_id: req.body.class_type_id, class_id: req.body.class_id, credits: req.body.credits });
+        } else {
+          const subscriptions = req.session.user.customer_id ? await stripe.subscriptions.list({
+            customer: req.session.user.customer_id
+          }) : { data: null };
 
-        req.session.history = updateHistory(req.session.history, 'purchase/');
-        res.render('../../client/views/pages/purchase', { user: req.session.user, subscriptions: subscriptions.data, message: `You do not have enough credits to register for this class. You need ${req.body.credits - req.session.user.credits} more to register.`, stripe_pk: process.env.STRIPE_API_PUBLIC_KEY, credit_cost: { one_time: process.env.ONE_TIME_CREDIT_COST_CENTS, sub_option_one: process.env.SUB_OPTION_ONE_CREDIT_COST_CENTS, sub_option_two: process.env.SUB_OPTION_TWO_CREDIT_COST_CENTS }});
+          req.session.history = updateHistory(req.session.history, 'purchase/');
+          res.render('../../client/views/pages/purchase', { user: req.session.user, subscriptions: subscriptions.data, message: `You do not have enough credits to register for this class. You need ${req.body.credits - req.session.user.credits} more to register.`, stripe_pk: process.env.STRIPE_API_PUBLIC_KEY, credit_cost: { one_time: process.env.ONE_TIME_CREDIT_COST_CENTS, sub_option_one: process.env.SUB_OPTION_ONE_CREDIT_COST_CENTS, sub_option_two: process.env.SUB_OPTION_TWO_CREDIT_COST_CENTS }});
+        }
+      } else {
+        res.redirect('/');
       }
     } else {
       req.session.history = updateHistory(req.session.history, 'account/login');
