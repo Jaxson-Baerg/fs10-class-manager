e241cc97f062922e7400599f738809e6e0928384
Andrew Baerg <andrew@pullingshots.ca>
2023-01-25 20:36:50 -0700

    Move testimonials and about to homepage, add hero

--- server/app.js
+++ server/app.js
@@ -10,8 +10,6 @@ const homeRouter = require('./routes/home');
 const scheduleRouter = require('./routes/schedule');
 const accountRouter = require('./routes/account');
 const purchaseRouter = require('./routes/purchase');
-const aboutRouter = require('./routes/about');
-const testimonialsRouter = require('./routes/testimonials');
 const adminRouter = require('./routes/admin');
 const { initScheduledJobs } = require('./helpers/scheduledFunctions');
 
@@ -40,8 +38,6 @@ app.use('/', homeRouter);
 app.use('/schedule', scheduleRouter);
 app.use('/account', accountRouter);
 app.use('/purchase', purchaseRouter);
-app.use('/about', aboutRouter);
-app.use('/testimonials', testimonialsRouter);
 app.use('/admin', adminRouter);
 
 // Run node-cron init function
