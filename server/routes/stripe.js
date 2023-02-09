d75575cb0c193194ae0b3370b64f313db5e04bd0
Andrew Baerg <baerg@dev.pullingshots.ca>
2023-02-08 21:55:54 -0700

    Stripe requires that you send a 200 response to a webhook request

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -19,15 +19,19 @@ router.post('/', async (req, res) => {
       case 'invoice.paid':
         console.log(chalk.green('Subscription payment success'));
         addSubscriptionCredits(event.data.object);
+        res.status(200).json({ message: 'Subscription payment processed' });
         break;
       case 'invoice.payment_failed':
         console.log(chalk.red('Subscription payment failed'));
+        res.status(200).json({ message: 'Unhandled event type: invoice.payment_failed' });
         break;
       case 'invoice.upcoming':
         console.log(chalk.yellow('Subscription payment upcoming.'));
+        res.status(200).json({ message: 'Unhandled event type: invoice.upcoming' });
         break;
       default:
         console.log(`Unhandled event type: ${event.type}`);
+        res.status(200).json({ message: `Unhandled event type: ${event.type}` });
     }
   } catch(err) {
     console.log(chalk.red.bold(`Error (${err.status}): `) + " " + err.message);
