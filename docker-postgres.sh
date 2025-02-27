d8b74e34624dc76863569d085a5618e3b941aa0d
Andrew Baerg <andrew@pullingshots.ca>
2025-02-26 20:22:36 -0700

    add README

--- docker-postgres.sh
+++ docker-postgres.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-echo "NOTE: You may need to run the following command to initialize the db"
-echo "  docker run --rm -e POSTGRES_PASSWORD=password -v `pwd`/data:/var/lib/postgresql/data postgres:alpine"
+echo "NOTE: You may need to initialize the database first, see README"
 docker run --rm --user "$(id -u):$(id -g)" -v $PWD/data:/var/lib/postgresql/data -p 5432:5432 postgres:alpine
